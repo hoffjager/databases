e96d078c7551b56b0382ad8485b60c07028e48e2
Jonathon <jonnygrealish64@gmail.com>
2023-02-27 10:50:55 +0000

    deleting defunct files to tidy repo

--- create_table.sql
+++
@@ -1,9 +0,0 @@
-﻿USE playstation;
-GO
-CREATE TABLE Finances (
-	Game varchar(255),
-	Price int,
-	Paid int,
-	Savings int,
-	Compatibility varchar(255)
-);
--- insert_singlevalue.sql
+++
@@ -1,2 +0,0 @@
-﻿INSERT dbo.Finances (Id, Game, Price, Paid, Savings)
-VALUES (1,'A Bugs Life',€1.49,€1.49,€0.00);
