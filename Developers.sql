790afe6283fbb57f0674dd9191839c9f909693aa
Jonathon <jonnygrealish64@gmail.com>
2023-03-10 16:42:25 +0000

    editing csv files to add years to original PS1 titles from the 1990s

---
+++ Developers.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Game] VARCHAR(MAX) NULL, 
+    [Console] VARCHAR(MAX) NULL, 
+    [Developer] VARCHAR(MAX) NULL, 
+    [Country] VARCHAR(MAX) NULL
+)
